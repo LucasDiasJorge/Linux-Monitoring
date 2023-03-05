3e634ffa2683a53084d2c5aab3fea060f59bea70
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-05 13:58:13 -0300

    Using more sysinfo function

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Linux monitoring
 
- Esse código utiliza a função "sysinfo" para coletar as informações de carga média do sistema nos últimos 1, 5 e 15 minutos e apresenta essas informações em tempo real na saída padrão. O loop infinito com um "sleep" de 1 segundo garante que as informações serão atualizadas a cada segundo.
+ Esse código utiliza a função "sysinfo" para coletar as informações de carga média do sistema nos últimos 1, 5 e 15 minutos e apresenta essas informações em tempo real na saída padrão. O loop infinito com um "sleep" de 1 segundo garante que as informações serão atualizadas a cada segundo. Além disso, trazendo informações de uso de memória, processos totais do sistema e tempo ligado.
--- main.c
+++ main.c
@@ -3,15 +3,32 @@
 #include <unistd.h>
 #include <sys/sysinfo.h>
 
+long castTime(long sec){
+    return sec/60;
+}
+
+long castRam(long b){
+    return b/1000000;
+}
+
 int main()
 {
     struct sysinfo si;
 
     while (1) {
+        
         if (sysinfo(&si) != 0) {
             printf("Erro ao coletar informações de sistema\n");
             exit(1);
         }
+
+        printf("ligado por %li minutos\n",castTime(si.uptime));
+
+        printf("Todos os processos: %d\n",si.procs);
+
+        printf("Memória total: %limb\n",castRam(si.totalram));
+
+        printf("Memória livre: %limb\n",castRam(si.freeram));
 
         printf("Carga média do sistema nos últimos 1, 5 e 15 minutos: %.2f, %.2f, %.2f\n",
             (float)si.loads[0] / (1 << SI_LOAD_SHIFT),
