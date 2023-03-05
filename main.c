d650e23134fa6c2666a7128eaba972e7a214468d
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-03-04 23:25:21 -0300

    Using sysinfo
    A carga média, em relação ao monitoramento de desempenho de um sistema Linux, é uma medida da quantidade média de trabalho que o sistema está realizando em um determinado período de tempo. Essa medida é obtida a partir da análise da quantidade de processos e threads ativos no sistema, levando em consideração também a quantidade de CPUs disponíveis e a utilização da memória RAM. A carga média é um indicador útil para entender a capacidade de processamento do sistema e identificar gargalos de desempenho.

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
 
-int main(){
+int main()
+{
+    struct sysinfo si;
 
-    while (true)
-    {
-        system("date > data.txt");
-        system("cat data.txt >> memory.txt");
-        system("free -h > data.txt");
-        system("cat data.txt >> memory.txt");
-        sleep(1800);
+    while (1) {
+        if (sysinfo(&si) != 0) {
+            printf("Erro ao coletar informações de sistema\n");
+            exit(1);
+        }
+
+        printf("Carga média do sistema nos últimos 1, 5 e 15 minutos: %.2f, %.2f, %.2f\n",
+            (float)si.loads[0] / (1 << SI_LOAD_SHIFT),
+            (float)si.loads[1] / (1 << SI_LOAD_SHIFT),
+            (float)si.loads[2] / (1 << SI_LOAD_SHIFT));
+
+        sleep(1);
     }
 
     return 0;
-
 }
