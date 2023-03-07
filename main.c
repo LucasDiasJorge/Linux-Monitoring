6caf8b4d2933fbace70231cecb4d4096bdf14abe
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2023-03-07 14:42:44 -0300

    get cpu frequency

--- main.c
+++ main.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/sysinfo.h>
 
-long castTime(long sec){
-    return sec/60;
-}
+int main(){
+    
+    FILE *fp;
+    char buffer[1024];
+    float frequency = 0.0;
 
-long castRam(long b){
-    return b/1000000;
-}
+    fp = fopen("/proc/cpuinfo", "r");
 
-int main()
-{
-    struct sysinfo si;
-
-    while (1) {
-        
-        if (sysinfo(&si) != 0) {
-            printf("Erro ao coletar informações de sistema\n");
-            exit(1);
-        }
-
-        printf("ligado por %li minutos\n",castTime(si.uptime));
-
-        printf("Todos os processos: %d\n",si.procs);
-
-        printf("Memória total: %limb\n",castRam(si.totalram));
-
-        printf("Memória livre: %limb\n",castRam(si.freeram));
-
-        printf("Carga média do sistema nos últimos 1, 5 e 15 minutos: %.2f, %.2f, %.2f\n",
-            (float)si.loads[0] / (1 << SI_LOAD_SHIFT),
-            (float)si.loads[1] / (1 << SI_LOAD_SHIFT),
-            (float)si.loads[2] / (1 << SI_LOAD_SHIFT));
-
-        sleep(1);
+    if(fp == NULL){
+        printf("Erro ao abrir o arquivo");
+        return -1;
     }
 
+    while(fgets(buffer, sizeof(buffer), fp)){
+        if(sscanf(buffer,"cpu MHz : %f", &frequency) == 1){
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Frequencia = %.2f", frequency);
+
     return 0;
+
 }
